8056fe828b2edb4716b28fec351f3794f777f1fd
Vladislav <xakeppok10@gmail.com>
2024-04-17 13:01:28 +0300

    fix: port parse

--- lib/src/wifi-printer.model.dart
+++ lib/src/wifi-printer.model.dart
@@ -6,8 +6,12 @@ class WifiPrinterModel {
   final int port;
 
   static WifiPrinterModel fromJson(Map<String, dynamic> json) =>
-      WifiPrinterModel(json['address'] as String, json['name'] as String,
-          int.tryParse(json['port']) ?? 0000);
+      WifiPrinterModel(
+          json['address'] as String,
+          json['name'] as String,
+          json['port'] is int
+              ? json['port']
+              : int.tryParse(json['port']) ?? 6101);
 
   Map<String, dynamic> toJson() =>
       <String, dynamic>{'address': address, 'name': name, 'port': port};
